b2f2f6b3d58a921e9fdc1688a1d6d1935d845c9c
Krishna-Dhawan <dhawan.krish@gmail.com>
2025-02-27 20:00:17 +0530

    Update Group21x.py

--- Group21x.py
+++ Group21x.py
@@ -58,8 +58,8 @@ def corrupt_crop(images):
     for img in images:
         x = random.randint(0, 5)
         y = random.randint(0, 5)
-        corrupted_img = [[pixel if x <= i < x + 10 and y <= j < y + 10 else 0 for j, pixel in enumerate(row)] for i, row
-                         in enumerate(img)]
+        corrupted_img = [[pixel if x <= i < x + 10 and y <= j < y + 10 else 0 for j, pixel in enumerate(row)]
+                         for i, row in enumerate(img)]
         corrupted_images.append(corrupted_img)
     return corrupted_images
 
@@ -125,13 +125,15 @@ def main():
     W = train_hopfield(dataset)
 
     probabilities = range(2, 10)
-    convergence_rates_flip = []
+    convergence_rates_flip_sync = []
+    convergence_rates_flip_async = []
     steps_histogram_data = {}
 
     # get data for successful converges when the pixels are flipped
     # with probability prob for each prob
     for prob in probabilities:
-        successful_convergences = 0
+        successful_convergences_sync = 0
+        successful_convergences_async = 0
         steps_data = []
         run = 0
         # 20 batches for each probability
@@ -145,22 +147,26 @@ def main():
                 corrected_sync, steps_sync = correct_synchronous(img, W)
                 corrected_async, steps_async = correct_asynchronous(img, W)
                 if compare_image(original, corrected_sync):
-                    successful_convergences += 1
+                    successful_convergences_sync += 1
                     steps_data.append(steps_sync)
                     print(
-                        f"Probability : {prob * 0.1} Run {run} Image {imgn} successfully converges in {steps_sync} Synchronous steps")
+                        f"Probability : {prob * 0.1} Run {run} Image {imgn} successfully converges in \
+                                        {steps_sync} Synchronous steps")
                 if compare_image(original, corrected_async):
-                    successful_convergences += 1
+                    successful_convergences_async += 1
                     steps_data.append(steps_async)
                     print(
-                        f"Probability : {prob * 0.1} Run {run} Image {imgn} successfully converges in {steps_async} Asynchronous steps")
+                        f"Probability : {prob * 0.1} Run {run} Image {imgn} successfully converges in \
+                                        {steps_async} Asynchronous steps")
 
-        convergence_rates_flip.append(successful_convergences / (40 * len(dataset)))
+        convergence_rates_flip_sync.append(successful_convergences_sync / (20 * len(dataset)))
+        convergence_rates_flip_async.append(successful_convergences_async / (20 * len(dataset)))
         steps_histogram_data[prob] = steps_data
 
     # get data for successful converges when the images are cropped
     # for each image
-    successful_convergences = [0, 0, 0, 0]
+    sync_successful_convergences = [0, 0, 0, 0]
+    async_successful_convergences = [0, 0, 0, 0]
     for _ in range(20):
         corrupted_images = corrupt_crop(dataset)
         for img, original in zip(corrupted_images, dataset):
@@ -168,31 +174,42 @@ def main():
             corrected_async, steps_async = correct_asynchronous(img, W)
             for i in range(4):
                 if corrected_sync[i] == original[i]:
-                    successful_convergences[i] += 1
+                    sync_successful_convergences[i] += 1
                 if corrected_async[i] == original[i]:
-                    successful_convergences[i] += 1
+                    async_successful_convergences[i] += 1
 
     # plot data for converges when images are cropped
     plt.figure(figsize=(8, 5))
-    plt.bar([img_ for img_ in ["circle", "cross", "plus", "square"]], successful_convergences, color='red')
+    plt.bar([img_ for img_ in ["circle", "cross", "plus", "square"]],
+            [i/40 for i in sync_successful_convergences],
+            color='red')
     plt.xlabel("Correct Image")
-    plt.ylabel("Converged Runs")
-    plt.title("Convergence for cropped images vs Correct Image")
+    plt.ylabel("Converged Runs (Synchronous Updates)")
+    plt.title("Convergence for cropped images vs Correct Image (Synchronous Updates)")
+    plt.show()
+
+    plt.figure(figsize=(8, 5))
+    plt.bar([img_ for img_ in ["circle", "cross", "plus", "square"]],
+            [i / 40 for i in async_successful_convergences],
+            color='red')
+    plt.xlabel("Correct Image")
+    plt.ylabel("Converged Runs (Asynchronous Updates)")
+    plt.title("Convergence for cropped images vs Correct Image (Asynchronous Updates)")
     plt.show()
 
     # plot data for converges when pixels are randomly flipped
     plt.figure(figsize=(8, 5))
-    plt.bar([("0." + str(p)) for p in probabilities], convergence_rates_flip, color='blue')
+    plt.bar([("0." + str(p)) for p in probabilities], convergence_rates_flip_sync, color='blue')
     plt.xlabel("Flip Probability")
-    plt.ylabel("Fraction of Correctly Converged Runs")
-    plt.title("Convergence Rate vs Flip Probability")
+    plt.ylabel("Fraction of Correctly Converged Runs (Synchronous Updates)")
+    plt.title("Convergence Rate vs Flip Probability (Synchronous Updates)")
     plt.show()
 
     plt.figure(figsize=(8, 5))
-    plt.bar([("0." + str(p)) for p in probabilities], [1-val for val in convergence_rates_flip], color='blue')
+    plt.bar([("0." + str(p)) for p in probabilities], convergence_rates_flip_async, color='blue')
     plt.xlabel("Flip Probability")
-    plt.ylabel("Fraction of incorrectly/non-Converged Runs")
-    plt.title("Rate vs Flip Probability")
+    plt.ylabel("Fraction of Correctly Converged Runs (Asynchronous Updates)")
+    plt.title("Convergence Rate vs Flip Probability (Asynchronous Updates)")
     plt.show()
 
     # plot histograms for no. steps to convergence for each prob. of flip
